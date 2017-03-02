b56d5ed8ba61cb127f644f8f5908a631846deda3
Erik R. Jensen <ejensen@netradius.com>
2017-03-01 20:38:33 -0700

    Adding placeholder unit test

---
+++ src/test/java/email/truemark/PlaceholderTest.java
@@ -0,0 +1,15 @@
+package email.truemark;
+
+import org.junit.Test;
+
+/**
+ * @author Erik R. Jensen
+ */
+public class PlaceholderTest {
+
+	@Test
+	public void test() {
+		// TODO We need to add some real unit tests
+	}
+
+}
